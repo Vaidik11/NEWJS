063ca8ada097eefda1dc4aed2fa13610008121eb
Vaidik Shrivastava <89836524+Vaidik11@users.noreply.github.com>
2024-08-02 12:50:24 +0000

    SecondCommit

---
+++ .devcontainer/basics/var.js
@@ -0,0 +1,7 @@
+const accountId = 14455
+let accountEmail = "Vaidik@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
